d7d01db1b0df9fc46819c1924db330da03ca7ed6
Sang Ki Kwon <cranberrygame@yahoo.com>
2016-08-20 13:35:22 +0900

    Update AdMobOverlap.java

--- src/android/AdMobOverlap.java
+++ src/android/AdMobOverlap.java
@@ -454,7 +454,7 @@ public class AdMobOverlap implements PluginDelegate {
 			rewardedVideo = MobileAds.getRewardedVideoAdInstance(plugin.getCordova().getActivity());
 			//
 			//rewardedVideo.setAdUnitId(this.rewardedVideoAdUnit);
-			rewardedVideo.setAdListener(new MyRewardedVideoAdListener());					
+			rewardedVideo.setRewardedVideoAdListener(new MyRewardedVideoAdListener());	
 		}		
 		
 		AdRequest.Builder builder = new AdRequest.Builder();
